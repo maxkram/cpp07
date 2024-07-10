6104856180d57b20b1af45d51d25124700b22057
maxkram <max.kramer@live.com>
2024-07-10 03:36:42 -0400

    add ex02

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,72 +1,44 @@
 #pragma once
+
 #include <iostream>
+#include <string>
+#include <stdlib.h>																			
 
-#define D "\033[0m"
-#define GRAY "\033[90m"
-#define R "\033[31m"
-#define C "\033[36m"
-#define GREEN "\033[32m"
-#define Y "\033[33m"
-#define BLUE "\033[94m"
-#define M "\033[35m"
+template <class T>
+class Array{
+    private:
+        T *array;
+        unsigned int _size;
+    public:
+        Array():_size(0) {
+            array = new T[0];
+        };
+        Array(unsigned int n):_size(n) {
+            array = new T[_size];
+        };
+        Array (const Array&  other) {
+            *this  = other;
+        };
+        Array & operator = ( const Array & other ) {
+            if (this == &other)
+                return (*this);
+            this->_size = other._size;
+            this->array = new T[this->_size];
+            for (size_t i = 0; i < this->_size; i++)
+                this->array[i] = other.array[i];
+            return (*this);
+        };
 
-//Template class Array !!!
-template <typename T>
-class Array
-{
-private:
-	T *_array;
-	size_t _size;
-public:
-	//Default constructor
-	Array() : _array(NULL), _size(0) { std::cout << GREEN << "Default constructor called" << DEFAULT << std::endl;};
-
-	//Destructor
-	~Array() { delete[] _array; std::cout << RED << "Destructor called" << DEFAULT << std::endl; };
-
-	//Parameter constructor
-	Array(unsigned int n) : _array(new T[n]()), _size(n) { std::cout << CYAN << "Parameterized constructor called" << DEFAULT << std::endl; };
-
-	//Copy constructor
-	Array(const Array &other) : _array(new T[other._size]()), _size(other._size)
-	{
-		std::cout << BLUE << "Copy constructor called" << DEFAULT << std::endl;
-		for (size_t i = 0; i < _size; i++)
-			_array[i] = other._array[i];
-	};
-
-	//Assignment operator
-	Array &operator=(const Array &copy)
-	{
-		std::cout << YELLOW << "Assignment operator called" << DEFAULT << std::endl;
-		if (this == &copy)
-			return *this;
-		delete[] _array;
-		_array = new T[copy._size]();
-		_size = copy._size;
-		for (size_t i = 0; i < _size; i++)
-			_array[i] = copy._array[i];
-		return *this;
-	};
-
-	//check if the index is out of range
-	T &operator[](unsigned int i)
-	{
-		if (i >= _size)
-			throw std::out_of_range("Sorry. The index is out of the range");
-		return _array[i];
-	};
-
-	//returns the size of the array
-	size_t size() const { return _size; };
-
-	//Exception class
-	class OutOfRange : public std::exception
-	{
-		virtual const char *what() const throw()
-		{
-			return "Sorry. The index is outside of the range";
-		};
-	};
-
+        T &operator[](unsigned int i) {
+            if (i >= _size)
+                throw std::exception();
+            return (array[i]);
+        };
+        int size() const {
+            return (_size);
+        };
+        ~Array() {
+            if (_size > 0)
+                delete [] array;
+        };
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,54 +1,16 @@
-#include <iostream>
-#include <Array.hpp>
-#include <cstddef>
+#include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+int main(void)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+    Array<int> array(10);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
+    try {
+        array[6] = 8;
+        std::cout << array[6] << std::endl;
+        std::cout << array[50] << std::endl;
     }
-    try
-    {
-        numbers[-2] = 0;
+    catch(std::exception &e) {
+        std::cout << "Index out of bounds." << std::endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+    return (0);
 }
